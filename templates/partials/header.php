ab613b66f8936352ec1b68a6fbc1ff4f1d41739b
VinCeProg <cananes.vincent.cueto@gmail.com>
2025-05-24 16:54:51 +0800

    added partial and template for main page

---
+++ templates/partials/header.php
@@ -0,0 +1,10 @@
+<header>
+  <div class="title">
+    <a href="#"><img src="./img/barangay.svg" alt="Baranggay Logo"></a>
+    <div class="title-text">
+      <h1>Barangay PUP</h1>
+      <p>Barangay PUP, Sta. Mesa, Manila</p>
+    </div>
+  </div>
+  <a href="#" class="login-btn">Login/Sign-Up</a>
+</header>
---
+++ templates/partials/navbar.php
@@ -0,0 +1,8 @@
+<nav>
+  <ul>
+    <li><a href="#" class="links">Home</a></li>
+    <li><a href="#" class="links">Complaint/Request Submission</a></li>
+    <li><a href="#" class="links">Gallery</a></li>
+    <li><a href="#" class="links">Contacts</a></li>
+  </ul>
+</nav>
