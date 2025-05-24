63da5b7f8f5340be9dc3e7615959be8c8054e1cc
VinCeProg <cananes.vincent.cueto@gmail.com>
2025-05-24 17:16:22 +0800

    formatted main page

--- templates/main.php
+++ templates/main.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Brgy | Home</title>
-  <link rel="stylesheet" href="../public/css/shared-styles.css">
+  <link rel="stylesheet" href="/public/css/shared-styles.css">
 </head>
+
 <body>
   <?php require("partials/header.php") ?>
   <?php require("partials/navbar.php") ?>
 </body>
+
 </html>
