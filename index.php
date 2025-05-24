72089987b727e2fb02c7a76c19b86b66fa0e64a2
VinCeProg <cananes.vincent.cueto@gmail.com>
2025-05-24 16:55:20 +0800

    replaced with template

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php 
-  $name = "Vince";
+  require("templates/main.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Brgy | Main Page</title>
-</head>
-<body>
-  <h1><?php echo $name ?></h1>
-</body>
-</html>
