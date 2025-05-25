a59e2c33515c9ec940b8e3a4def091e44b18c843
VinCeProg <cananes.vincent.cueto@gmail.com>
2025-05-25 13:11:38 +0800

    added model for resident

---
+++ src/models/Resident.php
@@ -0,0 +1,31 @@
+<?php
+require_once('config/database.php');
+
+class Resident {
+  private $conn;
+
+  public function __construct($dbConnection) {
+    $this->conn = $dbConnection;
+  }
+
+  public function createResident($data) {
+    //hash password
+    $data['password'] = password_hash($data["password"], PASSWORD_BCRYPT);
+
+    $query = "INSERT INTO residents (FirstName, LastName, Address, Email, PhoneNo, Password) VALUES (?, ?, ?, ?, ?, ?);";
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->bind_param("ssssss", 
+                      $data['first_name'],
+                      $data['last_name'],
+                      $data['address'],
+                      $data['email'],
+                      $data['phone_no'],
+                      $data['password']
+    );
+
+    return $stmt->execute();
+  }
+}
+
+?>
