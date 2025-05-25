838b27ce73449314b28f758e2b24e50013bd3c98
VinCeProg <cananes.vincent.cueto@gmail.com>
2025-05-25 13:11:54 +0800

    format refactor

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
-  $host = "localhost";
-  $user = "root";
-  $password = "";
-  $database = "brgy_tx_system";
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "brgy_tx_system";
 
-  $conn = new mysqli($host,$user,$password,$brgy_tx_system);
+$conn = new mysqli($host, $user, $password, $brgy_tx_system);
 
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 ?>
