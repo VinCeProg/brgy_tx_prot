a63355f40dbe2614d14bee23e7fa6ec97760cb60
VinCeProg <cananes.vincent.cueto@gmail.com>
2025-05-26 14:21:18 +0800

    added footer styling to head

--- views/partials/html.head.php
+++ views/partials/html.head.php
@@ -4,6 +4,6 @@
   <title>Brgy | Login</title>
   <link rel="icon" type="image/svg" href="/brgy_tx_prot/public/images/barangay.svg">
   <link rel="stylesheet" href="/brgy_tx_prot/public/css/index.shared-styles.css">
-  <link rel="stylesheet" href="/brgy_tx_prot/public/css/index.header.css"> <!-- Header -->
   <link rel="stylesheet" href="/brgy_tx_prot/public/css/index.navbar.css"> <!-- Navbar -->
+  <link rel="stylesheet" href="/brgy_tx_prot/public/css/index.footer.css">
 </head>
