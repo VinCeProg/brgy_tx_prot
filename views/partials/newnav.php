b9bdd9bc9cf75c78a3ed90c87e710a4fe732f30b
VinCeProg <cananes.vincent.cueto@gmail.com>
2025-05-26 14:53:35 +0800

    new nav for logged in user

---
+++ views/partials/newnav.php
@@ -0,0 +1,23 @@
+<header>
+  <div class="title">
+    <a href="#"><img src="/brgy_tx_prot/public/images/barangay.svg" alt="Baranggay Logo" /></a>
+    <!-- <div class="title-text">
+          <h1>Barangay PUP</h1>
+          <p>Barangay PUP, Sta. Mesa, Manila</p>
+        </div> -->
+  </div>
+  <nav class="navbar">
+    <ul>
+      <li><a href="/brgy_tx_prot/index.php">HOME</a></li>
+      <li><a href="#">ABOUT</a></li>
+      <li><a href="#">GALLERY</a></li>
+      <li><a href="#">CONTACTS</a></li>
+      <li><a href="#">COMPLAINTS</a></li>
+    </ul>
+    <div class="login-wrapper">
+      <a href="/brgy_tx_prot/src/controllers/residentLogoutController.php" class="login-btn">LOG OUT</a>
+    </div>
+  </nav>
+  <button class="menu-btn" onclick="toggleMenu()">☰</button>
+</header>
+<script src="public/js/mobile-menu.js"></script>
