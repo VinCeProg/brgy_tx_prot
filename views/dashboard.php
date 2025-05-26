6594a00beeb138df4dae4497928ff5aaa4d33b1c
VinCeProg <cananes.vincent.cueto@gmail.com>
2025-05-26 14:18:12 +0800

    refactor html head

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Brgy | Home</title>
-  <link rel="icon" type="image/svg" href="public/images/barangay.svg">
-  <link rel="stylesheet" href="public/css/shared-styles.css">
-</head>
+<?php
+  require("../functions.php");
+  require("partials/html.head.php"); 
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+  // dd($_SERVER);
+?>
 
 <body>
   <?php require("partials/navbar.php") ?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,7 +1,9 @@
 <?php
+  require("../functions.php");
   require("partials/html.head.php"); 
   error_reporting(E_ALL);
   ini_set('display_errors', 1);
+  // dd($_SERVER);
 ?>
 
 <body>
