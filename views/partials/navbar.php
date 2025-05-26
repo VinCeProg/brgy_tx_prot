5f1dbbb6a19596a7f0de97eeb4de24c9f7250af7
VinCeProg <cananes.vincent.cueto@gmail.com>
2025-05-26 14:53:21 +0800

    updated navbar to new nav

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,21 +1,22 @@
 <header>
   <div class="title">
-    <a href="#"><img src="/brgy_tx_prot/public/images/barangay.svg" alt="Baranggay Logo"></a>
-    <div class="title-text">
-      <h1>Barangay PUP</h1>
-      <p>Barangay PUP, Sta. Mesa, Manila</p>
-    </div>
+    <a href="#"><img src="/brgy_tx_prot/public/images/barangay.svg" alt="Baranggay Logo" /></a>
+    <!-- <div class="title-text">
+          <h1>Barangay PUP</h1>
+          <p>Barangay PUP, Sta. Mesa, Manila</p>
+        </div> -->
   </div>
   <nav class="navbar">
     <ul>
       <li><a href="/brgy_tx_prot/index.php">HOME</a></li>
+      <li><a href="#">ABOUT</a></li>
       <li><a href="#">GALLERY</a></li>
       <li><a href="#">CONTACTS</a></li>
-      <li><a href="#">ABOUT</a></li>
     </ul>
-    <a href="/brgy_tx_prot/views/login.php" class="login-btn">Login/Sign-Up</a>
+    <div class="login-wrapper">
+      <a href="/brgy_tx_prot/views/login.php" class="login-btn">LOG IN</a>
+    </div>
   </nav>
   <button class="menu-btn" onclick="toggleMenu()">☰</button>
 </header>
-
 <script src="public/js/mobile-menu.js"></script>
