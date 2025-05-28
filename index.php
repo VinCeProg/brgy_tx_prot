615bf6ba88b3490c2dd285e39ca9e8f3b559ec22
VinCeProg <cananes.vincent.cueto@gmail.com>
2025-05-28 10:31:56 +0800

    corrected link to dashboard if logged in

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start(); // Always start session before checking login status
 require("functions.php");
 // Redirect logged-in users to the dashboard
 if (isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in'] == true) {
-    header("Location: /brgy_tx_prot/views/dashboard.php");
+    header("Location: /brgy_tx_prot/views/dashboard/index.php");
     exit(); // Stop execution after redirect
 }
 
