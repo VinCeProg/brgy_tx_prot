8c79d4cd50387b52af19d22ffbafbf4ca5d9aca5
VinCeProg <cananes.vincent.cueto@gmail.com>
2025-05-30 13:29:07 +0800

    differentiate staff and resident session login

--- config/staff-auth.php
+++ config/staff-auth.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-if (!isset($_SESSION['is_logged_in']) || $_SESSION['is_logged_in'] !== true) {
+if (!isset($_SESSION['is_staff_logged_in']) || $_SESSION['is_staff_logged_in'] !== true) {
     header("Location: /brgy_tx_prot/views/helpdesk/login.php"); 
     exit();
 }
