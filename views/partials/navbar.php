70b82428a528d0c142672e0c3dcd88edd71b53a3
VinCeProg <cananes.vincent.cueto@gmail.com>
2025-06-24 12:36:25 +0800

    Add FAQ page and integrate with navigation menu

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -8,6 +8,7 @@
       <li><a href="#transparency-service">GALLERY</a></li>
       <li><a href="#headline">CONTACTS</a></li>
       <li><a href="#about-us">ABOUT</a></li>
+      <li><a href="frequently-asked-questions.php">FAQ</a></li>
     </ul>
     <div class="login-wrapper" title="Login">
       <a href="/brgy_tx_prot/views/login.php" class="login-btn">LOG IN</a>
